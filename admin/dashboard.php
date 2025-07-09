cc07ea20cabda6250344a58c30b0f1eedea53b65
swarna38 <swarna.sutradhar.info@gamil.com>
2025-07-09 16:00:04 +0600

    dashboard done

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,18 +2,33 @@
     require_once '../config/connection.php';
     require_once '../includes/header.php';
 
+    if(!isset($_SESSION['user_id'])){
+        header("Location: ../auth/login.php");
+        exit;
+    }else{
+
+        $sql = $pdo->prepare("SELECT posts.*,
+        users.name AS author, categories.name as category
+        FROM posts
+        LEFT JOIN users ON posts.user_id= users.id
+        LEFT JOIN categories  ON posts.category_id = categories.id 
+        ORDER BY posts.created_at DESC");
+
+        $sql->execute();
+        $posts = $sql->fetchAll();
+    }
+
 ?>
 
 
-
-<h3 class="mb-3">All Posts</h3>
-<p>
+<h3 class="mb-3 fw-bold text-center mt-4">All Posts</h3>
+<p class="text-center">
     <a href="add_post.php" class="btn btn-success mb-3">+ Add New Post</a>
 </p>
 
-<div class="table-responsive">
+<div class="table-responsive container">
     <table class="table table-bordered table-striped align-middle text-center">
-        <thead class="table-dark">
+        <thead class="table-light">
             <tr>
                 <th>Title</th>
                 <th>Author</th>
@@ -22,14 +37,22 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach($posts as $post): ?>
-            <tr>
-                <td><?= htmlspecialchars($post['title']) ?></td>
-                <td><?= htmlspecialchars($post['author']) ?></td>
-                <td><?= htmlspecialchars($post['category']) ?></td>
-                <td><?= date('d M Y', strtotime($post['created_at'])) ?></td>
-            </tr>
-            <?php endforeach; ?>
+            <?php if($posts){ ?> 
+                <?php foreach($posts as $post){ ?>
+                    <tr>
+                        <td><?= htmlspecialchars($post['title']) ?></td>
+                        <td><?= htmlspecialchars($post['author']) ?></td>
+                        <td><?= htmlspecialchars($post['category']) ?></td>
+                        <td><?= date('d M Y', strtotime($post['created_at'])) ?></td>
+                    </tr>
+                <?php } ?>
+            <?php }else{ ?>
+                 <tr>
+                    <td colspan="4" class="text-center text-muted ">
+                    You have no posts yet. <a href="add_post.php">Add one</a>
+                    </td>
+                </tr>
+            <?php } ?>    
         </tbody>
     </table>
 </div>
